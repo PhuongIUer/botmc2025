08a6ff7bdf740906028890ff89b49d148e7c6f95
PhuongIUer <miphuong1109@gmail.com>
2025-08-26 17:12:44 +0700

    update rec

--- index.js
+++ index.js
@@ -194,11 +194,13 @@ function setupBotEvents(bot) {
   bot.on('kicked', reason => {
     console.log(`[${bot.username}] Bị kick:`, reason)
     resetAllBots()
-})
+  })
+  
   bot.on('error', err => {
-  console.log(`[${bot.username}] Lỗi:`, err) 
-  resetAllBots()
-})
+    console.log(`[${bot.username}] Lỗi:`, err)
+    resetAllBots()
+  })
+
   bot.on('end', () => console.log(`[${bot.username}] Đã ngắt kết nối`))
 }
 
@@ -209,9 +211,9 @@ botConfigs.forEach((config, index) => {
 })
 
 // ========== THIẾT LẬP RESET ĐỊNH KỲ 40 PHÚT ==========
-resetIntervalId = setInterval(() => {
-  resetAllBots()
-}, 41 * 60 * 1000) // 40 phút
+// resetIntervalId = setInterval(() => {
+//   resetAllBots()
+// }, 41 * 60 * 1000) // 40 phút
 
 // ========== XỬ LÝ TẮT SCRIPT ==========
 process.on('SIGINT', () => {
